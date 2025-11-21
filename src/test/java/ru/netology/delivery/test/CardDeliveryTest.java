f90a0773dfdd268de2e5d0f04a2ca2ad880b310c
Nata test <loginatom@gmail.com>
2025-11-21 13:59:44 +0500

    Add listener in CardDeliveryTest

--- src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
@@ -2,6 +2,11 @@ package ru.netology.delivery.test;
 
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -10,6 +15,19 @@ import ru.netology.delivery.data.DataGenerator;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
+
+    //добавляем листенер в тестовый класс перед выполнением тестов
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    //удаляем листенер после выполнения всех тестов
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
 
     @BeforeEach
     void setup() {
